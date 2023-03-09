97d861a5d5e809a79bfd8e1fa774b9bdd6712d6d
harixraza <106744750+harixraza@users.noreply.github.com>
2023-03-09 13:02:54 +0500

    secrete added

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,14 @@
+
 module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'GVPlEbvwyw7GyQO0/pRq5g=='),
   },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
-    },
+  auth: {
+    secret: env('ADMIN_JWT_SECRET', 'ecRdU72doMtcD6NGYsFaYQ=='),
+  },
+  transfer: { 
+    token: { 
+      salt: env('TRANSFER_TOKEN_SALT', 'kxFYaIngXXjRHj22iLMsiQ=='),
+    } 
   },
 });
